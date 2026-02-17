4892556d84ec10d680e1a08d495853608358186f
David Ingraham <davidingrahamf@gmail.com>
2026-02-17 12:50:06 -0500

    left a quick puzzle here

--- twoPointers/samedriection/moveZerosToBack.py
+++ twoPointers/samedriection/moveZerosToBack.py
@@ -2,7 +2,9 @@
 
 
 
+#question for next time you look at this:
 
+#excatly one charecter is off here. find and fix it 
 
 input = [0,0,0,1,0,2,0,0,0,7,0,0]
 print(f"pre processed: {input}")
@@ -14,7 +16,7 @@ while f < len(input) -1:
 
 	f = f +1
 
-	if input[f] != 0:
+	if input[f] == 0:
 		input[s], input[f] = input[f], input[s]
 		s = s +1
 
