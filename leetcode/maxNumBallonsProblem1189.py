6ae3b92aa09a1b507f836c505dcbf64db272d512
David Ingraham <davidingrahamf@gmail.com>
2026-06-22 14:56:20 -0400

    not working, the check if instance is same as word fails bc char are in diff order, places.

---
+++ leetcode/maxNumBallonsProblem1189.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        word = balloon
+        instance = 
+        count = 0
+
+        for char in text:
+            if char in word:
+                instance = instance + char
+                text = text.replace(char, )
+                print(instance)
+                print(text)
+            
+            if instance == word:
+                count +=1
+                instance = 
+        return count
+            
+
